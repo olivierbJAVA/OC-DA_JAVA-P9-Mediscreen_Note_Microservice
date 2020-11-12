b1db31cbbb94d69adee969590f18981b7e780462
olivier.bonheur <olivier.bonheur@gmail.com>
2020-11-12 01:13:13 +0100

    Updating NoteApplication.java

--- src/main/java/com/mediscreen/note/NoteApplication.java
+++ src/main/java/com/mediscreen/note/NoteApplication.java
@@ -3,6 +3,9 @@ package com.mediscreen.note;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Class in charge of launching the Note Microservices in charge of managing note data.
+ */
 @SpringBootApplication
 public class NoteApplication {
 
