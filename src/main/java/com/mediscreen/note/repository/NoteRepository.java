ef10490faebb3ffe7b5eb8f36aad4398f49517c9
olivier.bonheur <olivier.bonheur@gmail.com>
2020-11-12 01:12:24 +0100

    Adding NoteRepository.java

---
+++ src/main/java/com/mediscreen/note/repository/NoteRepository.java
@@ -0,0 +1,10 @@
+package com.mediscreen.note.repository;
+
+import com.mediscreen.note.domain.Note;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * Interface extending the MongoRepository interface to manage CRUD methods for Note entities, using Spring Data.
+ */
+public interface NoteRepository extends MongoRepository<Note, String> {
+}
