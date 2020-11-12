6511efc6ccd36abe8670b0d688d0c75974c67170
olivier.bonheur <olivier.bonheur@gmail.com>
2020-11-12 20:37:05 +0100

    Work on Repository layer

--- src/main/java/com/mediscreen/note/repository/NoteRepository.java
+++ src/main/java/com/mediscreen/note/repository/NoteRepository.java
@@ -7,4 +7,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
  * Interface extending the MongoRepository interface to manage CRUD methods for Note entities, using Spring Data.
  */
 public interface NoteRepository extends MongoRepository<Note, String> {
+
+    Note findByPatientLastNameAndPatientFirstName (String patientLastName, String patientFirstName);
+
 }
