928172359941d77d361682037972d8d59a85f017
olivier.bonheur <olivier.bonheur@gmail.com>
2020-11-12 01:12:47 +0100

    Adding RepositoryPopulator.java

---
+++ src/main/java/com/mediscreen/note/configuration/RepositoryPopulator.java
@@ -0,0 +1,24 @@
+package com.mediscreen.note.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
+
+/**
+ * Configuration Class used to populate the database with initial data.
+ */
+@Profile("dev")
+@Configuration
+public class RepositoryPopulator {
+
+    @Bean
+    public Jackson2RepositoryPopulatorFactoryBean getRespositoryPopulator() {
+        Jackson2RepositoryPopulatorFactoryBean factory = new Jackson2RepositoryPopulatorFactoryBean();
+        factory.setResources(new Resource[]{new ClassPathResource("data.json")});
+        return factory;
+    }
+
+}
